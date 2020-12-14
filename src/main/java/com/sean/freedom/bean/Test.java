6c1e25b41a92dea0a0113c225638dd47a2002c89
comsean <sean6663@outlook.com>
2020-12-14 11:49:45 +0800

    新建工程

--- src/main/java/com/sean/freedom/bean/Test.java
+++ src/main/java/com/sean/freedom/bean/Test.java
@@ -2,6 +2,6 @@ package com.sean.freedom.bean;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello for main");
     }
 }
