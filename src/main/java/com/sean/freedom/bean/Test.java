a77ae850037569abef18527a9e0699f864fefcca
comsean <sean6663@outlook.com>
2020-12-14 11:09:12 +0800

    iter

--- src/main/java/com/sean/freedom/bean/Test.java
+++ src/main/java/com/sean/freedom/bean/Test.java
@@ -2,6 +2,10 @@ package com.sean.freedom.bean;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello for main");
+        System.out.println("hello");
+        int[] ints = {1,2,5,89,2,64};
+        for (int i : ints) {
+            System.out.println("#="+i);
+        }
     }
 }
